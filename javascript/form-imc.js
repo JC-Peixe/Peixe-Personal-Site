1296dbc4ff5faec59376ee14eb97c867efac0083
Peixee <Jc_px@hotmail.com>
2022-08-02 00:39:08 -0300

    Update form-imc.js
    Reduzindo form criando funções para os dados do formulario

--- javascript/form-imc.js
+++ javascript/form-imc.js
@@ -4,26 +4,33 @@ var botaoImc = document.querySelector(".enviar");
 function calculaImc(event){
     event.preventDefault();
     var form = document.querySelector("#calculo-imc");
-    
-    var nome = form.nome.value;
-    var idade = form.idade.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value.replace(',','.');
-    var imc = calculaImc(peso,altura);
-    
+    var cliente = obterFormulario(form);
+   
     var resultadoIMC = document.querySelector(".imc");
-    resultadoIMC.textContent = imc;
+    resultadoIMC.textContent = cliente.imc;
     
     var classificacao = document.querySelector('.classificacao');
-    classificacao.textContent = classificadorImc(imc);
+    classificacao.textContent = classificadorImc(cliente.imc);
 
     var riscos = document.querySelector('.risco');
-    riscos.textContent = identificadorRiscos(imc);
+    riscos.textContent = identificadorRiscos(cliente.imc);
     
-    resultadoIMC.style.color = corImc(imc);
-    classificacao.style.color = corImc(imc);
-    resultadoIMC.style.backgroundColor = corBgImc(imc);
-    classificacao.style.backgroundColor = corBgImc(imc);
-    riscos.style.backgroundColor = corBgImc(imc);
+    resultadoIMC.style.color = corImc(cliente.imc);
+    classificacao.style.color = corImc(cliente.imc);
+    resultadoIMC.style.backgroundColor = corBgImc(cliente.imc);
+    classificacao.style.backgroundColor = corBgImc(cliente.imc);
+    riscos.style.backgroundColor = corBgImc(cliente.imc);
+    form.reset();
 }
+function obterFormulario(form){
+    var cliente = {
+        nome:form.nome.value,
+        idade:form.idade.value,
+        peso:form.peso.value,
+        altura:form.altura.value.replace(',','.'),
+        imc:calculaImc(form.peso.value,form.altura.value)
+    }
+    return cliente
+}
+
 botaoImc.addEventListener('click', calculaImc)
